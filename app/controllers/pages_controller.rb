4346bf9b743e899001ee5f099d850776925df890
jeffhappily <goodboy96@live.com.my>
2016-10-10 11:45:34 +0800

    some more search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,12 +2,5 @@ class PagesController < ApplicationController
 	#before_action :authenticate_user!
 
 	def index
-		@filterrific = initialize_filterrific(
-        Package,
-        params[:filterrific],
-      ) or return
-      @packages = @filterrific.find.page(params[:page])
-
-      redirect_to packages_path
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,16 @@ Rails.application.routes.draw do
 
  get '/packages/subregion_options' => 'packages#subregion_options'
 
+
   devise_scope :user do
-  	post '/user/registrations/choose' => 'user/registrations#choose'
- 	end
+    post '/user/registrations/choose' => 'user/registrations#choose'
+  end
   devise_for :users, :controllers => {
-  	:registrations => "user/registrations"
+    :registrations => "user/registrations"
   }
 
+  post '/packages', to: 'packages#index'
+  post '/pages/search', to: 'packages#index'
 
   resources :users
 
