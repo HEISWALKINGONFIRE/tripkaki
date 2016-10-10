a4b77d5b5d62cf82b31d6404bd18d2f6493bed97
jeffhappily <goodboy96@live.com.my>
2016-10-10 10:14:07 +0800

    add search to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,12 @@ class PagesController < ApplicationController
 	#before_action :authenticate_user!
 
 	def index
+		@filterrific = initialize_filterrific(
+        Package,
+        params[:filterrific],
+      ) or return
+      @packages = @filterrific.find.page(params[:page])
+
+      redirect_to packages_path
 	end
 end
