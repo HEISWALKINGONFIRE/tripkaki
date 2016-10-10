4cf64691f86a1ea0db0e1d312790e94d6ccb9cae
waffle-cat <xyng735@gmail.com>
2016-10-10 12:30:22 +0800

    Minor change to seed.db to accommodate new database schema.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@ User.create(first_name: "Elon", last_name: "Musk", email: "elon@spacex.com", use
 User.create(first_name: "Richard", last_name: "Hendricks", email: "richard@piedpiper.com", username: "elonmusk", password: "richard")
 
 
-Package.create(title: "Tour of SpaceX", destination: "An inside look of how SpaceX works!", state: "California", country: "USA", day: 1, description: "An inside look of how SpaceX works!", accommodation: true, transportation: "bus", meal: "burgers", head: 10, public_price: 100, private_price: 1000, user_id: 1)
+Package.create(title: "Tour of SpaceX", destination: "An inside look of how SpaceX works!", state: "California", country: "USA", day: 1, description: "An inside look of how SpaceX works!", accommodation: true, transportation: "bus", meal: "burgers", head: 10, private_price: 1000, user_id: 1)
