28a7cdd59ada7bfd80af95bd456938da826e0f10
jeffhappily <goodboy96@live.com.my>
2016-10-11 11:37:19 +0800

    change price range

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -74,8 +74,8 @@ $(function() {
    
     $( "#slider-range" ).slider({
       range: true,
-      min: 42,
-      max: 4000,
+      min: 20,
+      max: 1000,
       values: [ 75, 300 ],
       slide: function( event, ui ) {
         $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
