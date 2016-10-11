f0a7434d8d1a7a21ef18b2234904df647b470b10
Kc Lee <kc_lkc@live.com>
2016-10-11 10:50:24 +0800

    add return to top button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,7 @@
 //= require filterrific/filterrific-jquery
 //= require moment
 //= require daterangepicker
+//= require jquery.scrollTo
 
 $(function() {
   if ($("#filterrific_tour_type option:selected").val() == "private") {
@@ -49,5 +50,18 @@ $(function() {
       $("#filterrific_date_range").parent().show();
     }
   })
+
+	$(window).scroll(function() {
+	    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+	        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+	    }	else {
+	        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+	    } 
+	});
+	$('#return-to-top').click(function() {      // When arrow is clicked
+	    $('body,html').animate({
+	        scrollTop : 0                       // Scroll to top of body
+	    }, 500);
+	});
 });
 
